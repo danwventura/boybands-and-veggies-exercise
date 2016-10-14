39f813bac6ddbb84d8a0ae398c7ded5c2851ebc0
Dan Ventura <danwventura@icloud.com>
2016-10-13 22:00:23 -0500

    printing each item to respective div

--- boybands.js
+++ boybands.js
@@ -1,35 +1,47 @@
 var bands = ["Boyz II Men", "NSync", "New Kids on the Block", "98 Degrees", "One Direction"];
 var vegetables = ["Carrots", "Kale", "Zucchini", "Broccoli", "Squash"];
 
-// The number of loops to perform (what if the array changes?)
+// // The number of loops to perform (what if the array changes?)
 var loopCount = 5;
 
-// Keep track of which band we're on in the loop
-var currentBand = "";
+// // Keep track of which band we're on in the loop
+// var currentBand = "";
 
-// Keep track of which veggie we're on in the loop
-var currentVeggie = "";
+// // Keep track of which veggie we're on in the loop
+// var currentVeggie = "";
 
-// Get a reference to the appropriate DOM element for bands
-var bandElement = document.getElementById(???);
+// // Get a reference to the appropriate DOM element for bands
+// var bandElement = document.getElementById(???);
 
-// Get a reference to the appropriate DOM element for vegetables
-var veggieElement = ???;
+// // Get a reference to the appropriate DOM element for vegetables
+// var veggieElement = ???;
 
-// Start looping
-for (var loopTracker = 0; loopTracker < loopCount; loopTracker += 1) {
+// // Start looping
+// for (var loopTracker = 0; loopTracker < loopCount; loopTracker += 1) {
 
-  // Add the band names into the correct <div>
-  currentBand = ???;
+//   // Add the band names into the correct <div>
+//   currentBand = ???;
 
 
-  // Add the veggie names into the correct <div>
-  currentVeggie = ???;
+//   // Add the veggie names into the correct <div>
+//   currentVeggie = ???;
 
-}
+// }
 
 // Loop through the two arrays provided (bands and vegetables) and output each element in the arrays
 //  into their corresponding HTML <div> element. Ensure that each item is in a block element
 //   (e.g. li, div, p. etc...)
 
-for (var i = 0, i <= loopCount, i++)
+for (var i = 0; i <= loopCount; i++)
+{
+  bandElement = document.getElementById("boy-bands");
+  veggieElement = document.getElementById("vegetables");
+
+ var currentBand = bands[i];
+ var currentVeggie = vegetables[i]
+
+ bandElement.innerHTML += currentBand;
+ veggieElement.innerHTML += currentVeggie;
+
+
+}
