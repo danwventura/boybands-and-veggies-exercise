c16be8d440cf781ce27480a24f28d20a4c43bf8a
Dan <danwventura@gmail.com>
2016-11-27 18:50:52 -0600

    completed, comments, screenshot

--- README.md
+++ README.md
@@ -1,1 +1,26 @@
 # boybands-and-veggies-exercise
+
+
+
+### Description
+CREATED ARRAYS TO PRACTICE ITERATING/PRINTING STUFF TO DOM USING FOR LOOPS
+
+### SCREENSHOTS:
+
+
+https://github.com/danwventura/banking-form-exercise/blob/master/screenshots/bank_form_with_required_fields.png
+
+
+### How to run:
+
+```
+http-server -p XXXX
+```
+
+This will show at:
+http://localhost:XXXX
+
+
+### Contributors
+
+-[Dan Ventura](https://github.com/danwventura)
--- boybands.js
+++ boybands.js
@@ -1,35 +1,25 @@
+
+// Loop through the two arrays provided (bands and vegetables) and output each element
+// in the arrays into their corresponding HTML <div> element. Ensure that each item is in
+// a block element (e.g. li, div, p. etc...)
+
+//BUILDING ARRAYS TO ITERATE OVER/PRINT OUT
 var bands = ["Boyz II Men", "NSync", "New Kids on the Block", "98 Degrees", "One Direction"];
 var vegetables = ["Carrots", "Kale", "Zucchini", "Broccoli", "Squash"];
 
-// The number of loops to perform (what if the array changes?)
+// THE NUMBER OF TIMES TO RUN THE FOR LOOP
 var loopCount = 5;
 
-// Keep track of which band we're on in the loop
-var currentBand = "";
+//GRABBING DIV ELEMENTS BY ID TO ALTER INNERHTML IN OUR FOR LOOPS
+var bandElement = document.getElementById("boy_bands");
+var veggieElement = document.getElementById("vegetables");
 
-// Keep track of which veggie we're on in the loop
-var currentVeggie = "";
-
-// Get a reference to the appropriate DOM element for bands
-var bandElement = document.getElementById(???);
-
-// Get a reference to the appropriate DOM element for vegetables
-var veggieElement = ???;
-
-// Start looping
-for (var loopTracker = 0; loopTracker < loopCount; loopTracker += 1) {
-
-  // Add the band names into the correct <div>
-  currentBand = ???;
-
-
-  // Add the veggie names into the correct <div>
-  currentVeggie = ???;
-
+//FOR LOOP TO PRINT OUT EACH BAND NAME INTO THE DIV FOR BANDS
+for (var loopTracker = 0; loopTracker < loopCount; loopTracker++) {
+bandElement.innerHTML += ("<p>" + bands[loopTracker] + "</p>");
 }
 
-// Loop through the two arrays provided (bands and vegetables) and output each element in the arrays
-//  into their corresponding HTML <div> element. Ensure that each item is in a block element
-//   (e.g. li, div, p. etc...)
-
-for (var i = 0, i <= loopCount, i++)
+//FOR LOOP TO PRINT OUT EACH BAND NAME INTO THE DIV FOR VEGETABLES
+for (var loopCounter = 0; loopCounter < loopCount; loopCounter++){
+veggieElement.innerHTML += ("<p>" + vegetables[loopCounter] + "</p>");
+}
